78806542d15f7dc6eff499efb84eb691d54cf413
Tom <you@example.com>
2018-10-26 11:20:11 +0100

    added bookmark.rb and spec

--- app.rb
+++ app.rb
@@ -1,17 +1,11 @@
 require 'sinatra/base'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
   get '/bookmarks' do
-    'Bookmark Manager'
-  end
-  get '/bookmarks' do
-    @bookmarks = [
-        "http://www.makersacademy.com",
-        "http://www.destroyallsoftware.com",
-        "http://facebook.com"
-        ]
-
+    @bookmarks = Bookmark.all
     erb :'bookmarks/index'
   end
+
   run! if app_file == $0
 end
