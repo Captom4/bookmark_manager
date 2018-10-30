2ec987058ed9789bc00c69cba8c9ab80b8764daa
Tom <you@example.com>
2018-10-30 12:06:42 +0000

    added sql

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'sinatra/base'
 require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
+  get '/' do
+    'Bookmark Manager'
+  end
   get '/bookmarks' do
     @bookmarks = Bookmark.all
     erb :'bookmarks/index'
